6f2bd3d1df733b0320545813c5bfea1f0cf1f523
Alexandre Caussades <alexcaussades@gmail.com>
2025-02-02 20:18:02 +0100

    Mise à jour de la vue 'test' pour afficher la position géographique de l'utilisateur sur une carte et amélioration de la gestion des ressources dans l'en-tête.

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -6,26 +6,24 @@ use Carbon\Carbon;
 @extends("exention.header")
 @extends("exention.navbar")
 @section("content")
-
 <div class="container mt-5">
 <h3>
 <ul>
     <div class="col-md-8">
         <script>
-        document.addEventListener("DOMContentLoaded", function() {
             navigator.geolocation.getCurrentPosition(function(pos) {
+                console.log(pos)
                 var crd = pos.coords;
                 var iframe = document.querySelector("iframe");
-                iframe.src = `https://api.maptiler.com/maps/satellite/?key=kkpLQuTHyXEAqwhsuGBi#12.8/${crd.latitude}/${crd.longitude}`;
-            }, function(err) {
-                console.warn(`ERREUR (${err.code}): ${err.message}`);
-            }, {
-                timeout: 5000,
-                maximumAge: 0,
-            });
+                iframe.src = `https://api.maptiler.com/maps/satellite/?key=kkpLQuTHyXEAqwhsuGBi#16.35/${crd.latitude}/${crd.longitude}`;
+
+                
         });
         </script>
-
+        <div class="container">
+            <p id="latitude"></p>
+            <p id="longitude"></p>
+        </div>
         <iframe width="100%" height="500" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src=""></iframe>
 
     </div>
